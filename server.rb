9684455a8a20dcd2e1bf38df455a01aa7fe5efc2
Pete Nicholls <pete@metanation.com>
2010-05-17 16:30:54 +1200

    Added feed.

--- server.rb
+++ server.rb
@@ -3,18 +3,37 @@ require 'twitter'
 
 set :title, 'Morning, Christchurch'
 
+helpers do
+  def fetch_pics
+    tweets = Twitter::Search.new('morning OR christchurch from:danrandow').fetch.results
+    tweets.map do |t|
+      photo_urls = t.text.match(/http:\/\/yfrog\.com\/[a-z0-9]+/).to_a
+      next if photo_urls.empty?
+      {
+        :urls => photo_urls.map { |url| url << ':iphone' }, # to retreive yFrog iPhone-sized photos
+        :date => DateTime.parse(t.created_at),
+        :tweet => t.text
+      }
+    end.compact
+  end
+  
+  def site_url
+    "#{request.scheme}://#{request.host}/"
+  end
+  
+  def xmlschema_time(time)
+    time.strftime('%Y-%m-%dT%H:%M:%S%z')
+  end
+end
+
 get '/' do
-  tweets = Twitter::Search.new('morning OR christchurch from:danrandow').fetch.results
-  @pics = tweets.map do |t|
-    photo_urls = t.text.match(/http:\/\/yfrog\.com\/[a-z0-9]+/).to_a
-    next if photo_urls.empty?
-    {
-      :urls => photo_urls.map { |url| url << ':iphone' }, # to retreive yFrog iPhone-sized photos
-      :date => DateTime.parse(t.created_at),
-      :tweet => t.text
-    }
-  end.compact
+  @pics = fetch_pics
   haml :gallery
+end
+
+get '/feed.xml' do
+  @pics = fetch_pics
+  erb :feed
 end
 
 get '/stylesheet.css' do
